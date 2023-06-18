a76d975e65ae58ac6c721b6fb3d3244777d039b9
Nifaanya Priya <nifaanya.priya@paradigmcorp.com>
2023-06-18 17:15:21 +0000

    working code commit2 18/6

--- cypress/support/cmdAdvapro.js
+++ cypress/support/cmdAdvapro.js
@@ -3,37 +3,38 @@
 // Description: Login into the ADvapro application using Automation credentails 
 // Prerequisite: visit advapro url and click login button  
 Cypress.Commands.add('LoginAzure', function () {
-  cy.get('#logintag').click({timeout:2000});
-    var username='OpsAdvaQAAutomation@paradigmcorp.com',
-    password='CoffeeTrainEarthWater'
-    cy.origin(
-        'https://login.microsoftonline.com/',
-        {
-          args: {
-            username,
-            password
+    cy.get('#logintag').click({timeout:2000});
+      var username='OpsAdvaQAAutomation@paradigmcorp.com',
+      password='CoffeeTrainEarthWater'
+      cy.origin(
+          'https://login.microsoftonline.com/',
+          {
+            args: {
+              username,
+              password
+            },
           },
-        },
-        ({ username, password }) => {
-          
-          // 
-          //
-          cy.wait(7000)
-          cy.get('#idBtn_Back').click({timeout:7000})
-          cy.get('#otherTileText').click({timeout:3000})
-          cy.get('input[type="email"]').type(username, {
-            log: false,
+          ({ username, password }) => {
+            
+            // 
+            //
+            cy.wait(7000)
+            cy.get('#idBtn_Back').click({timeout:7000})
+            cy.get('#otherTileText').click({timeout:3000})
+            cy.get('input[type="email"]').type(username, {
+              log: false,
+            })
+            cy.screenshot()
+            cy.get('input[type="submit"]').click()
+            cy.wait(6000)
+            cy.get('input[type="password"]').type(password)
+                        
+                      cy.get('input[type="submit"]').click()
+                     // cy.screenshot("After Login screen ",{timeout:2000})
+  
+                   
           })
-          cy.screenshot()
-          cy.get('input[type="submit"]').click()
-          cy.wait(6000)
-          cy.get('input[type="password"]').type(password)
-                      
-                    cy.get('input[type="submit"]').click()
-                   // cy.screenshot("After Login screen ",{timeout:2000})
-
-                 
-        })
-})//end of LoginAzure cmd 
-
-
+  })//end of LoginAzure cmd 
+  
+  
+  
